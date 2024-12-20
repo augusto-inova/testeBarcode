a28daddd4aeb22ed22a60e9b58bb2b23534b13ba
augusto-inova <augusto.inovacaotec@gmail.com>
2024-12-20 11:45:09 -0300

    first commit

--- src/Barcode.tsx
+++ src/Barcode.tsx
@@ -1,15 +1,24 @@
+import { Button } from "@mui/material";
 import React, { useEffect } from "react";
 import BarcodeScannerComponent from "react-qr-barcode-scanner";
+import Condicional from "./Condicional";
 
 function BarCode() {
   const [data, setData] = React.useState("Not Found");
+  const [exibe, setexibe] = React.useState(false);
 
   useEffect(() => {
   console.log("valor"+data);
   }, [data]);
 
+  function exibirCamera(){
+    setexibe(!exibe);
+  }
   return (
     <>
+
+    <Button onClick={exibirCamera}>Exibir</Button>
+     <Condicional condicao={exibe}>
       <BarcodeScannerComponent
         width={500}
         height={500}       
@@ -22,6 +31,7 @@ function BarCode() {
             }
           }}
       />
+        </Condicional>
       <p>{data}</p>
     </>
   );
